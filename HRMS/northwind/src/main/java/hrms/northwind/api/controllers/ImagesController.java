1547a4016453fa07980e14d5f2db3bba67c99f82
Kutay Deniz Yazıcıoğlu <72033094+kdy34@users.noreply.github.com>
2021-06-11 21:39:35 +0300

    Update ImagesController.java

--- HRMS/northwind/src/main/java/hrms/northwind/api/controllers/ImagesController.java
+++ HRMS/northwind/src/main/java/hrms/northwind/api/controllers/ImagesController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import hrms.northwind.entities.concretes.Image;
 
 @RestController
 @RequestMapping(value = "/api/images")
+@CrossOrigin
 public class ImagesController {
 	
 	private CandidateImageService candidateImageService;
