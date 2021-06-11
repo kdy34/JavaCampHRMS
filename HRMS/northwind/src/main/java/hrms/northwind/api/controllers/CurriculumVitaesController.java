bd2089bd83fbbd3fc8f74d96ef4ab3929a7d37ac
Kutay Deniz Yazıcıoğlu <72033094+kdy34@users.noreply.github.com>
2021-06-11 21:37:41 +0300

    Update CurriculumVitaesController.java

--- HRMS/northwind/src/main/java/hrms/northwind/api/controllers/CurriculumVitaesController.java
+++ HRMS/northwind/src/main/java/hrms/northwind/api/controllers/CurriculumVitaesController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import hrms.northwind.entities.concretes.CurriculumVitae;
 
 @RestController
 @RequestMapping(value = "/api/curriculumVitaes")
+@CrossOrigin
 public class CurriculumVitaesController {
 	
 	private CurriculumVitaeService curriculumVitaeService;
