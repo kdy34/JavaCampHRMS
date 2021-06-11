704b48e1e5e3419608b7ad5c8961b458470c392e
Kutay Deniz Yazıcıoğlu <72033094+kdy34@users.noreply.github.com>
2021-06-11 21:35:21 +0300

    Update Candidate.java

--- HRMS/northwind/src/main/java/hrms/northwind/entities/concretes/Candidate.java
+++ HRMS/northwind/src/main/java/hrms/northwind/entities/concretes/Candidate.java
@@ -1,12 +1,12 @@
 package hrms.northwind.entities.concretes;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
-import hrms.northwind.entities.abstracts.Entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,10 +18,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper=false)
-public class Candidate extends User implements Entities{
-	
-	@Column(name="user_id")
-	private int userId;
+@PrimaryKeyJoinColumn(name = "id")
+public class Candidate extends User{
 	
 	@Column(name="first_name")
 	private String firstName;
@@ -29,21 +27,15 @@ public class Candidate extends User implements Entities{
 	@Column(name="last_name")
 	private String lastName;
 	
-	@Column(name="identity_number")
-	private String identityNumber;
-	
 	@Column(name="birth_year")
-	private Date birthYear;
+	private LocalDate birthYear = LocalDate.now();
 	
-	public Candidate(int id, String email, String password, boolean isActive, int userId, String firstName, String lastName, String identityNumber, Date birthYear) {
+	public Candidate(int id, String email, String password, boolean isActive, String firstName, String lastName) {
 		
 		super(id, email, password, isActive);
 		
-		this.userId = userId;
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.identityNumber = identityNumber;
-		this.birthYear = birthYear;
 	}
 	
 
