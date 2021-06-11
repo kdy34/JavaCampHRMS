a23c6c98020de0005fd521a28fccab57a6339eb9
Kutay Deniz Yazıcıoğlu <72033094+kdy34@users.noreply.github.com>
2021-06-11 21:41:02 +0300

    Update JobsControllers.java

--- HRMS/northwind/src/main/java/hrms/northwind/api/controllers/JobsControllers.java
+++ HRMS/northwind/src/main/java/hrms/northwind/api/controllers/JobsControllers.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import hrms.northwind.core.utilities.results.Result;
 
 @RestController
 @RequestMapping("/api/jobs")
+@CrossOrigin
 public class JobsControllers {
 	
 	private JobsService jobsService;
